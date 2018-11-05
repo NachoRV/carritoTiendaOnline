8d4db5def779f8c5b5af4c8107d58019274dd6a1
‘Ignacio’ <royovillanova.i@gmail.com>
2018-11-05 16:48:27 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ *  Variables:
+ *  Capturamso el div con id carrito y el div con id lista-cursos
+ * 
+ *  */
+ const carrito = document.getElementById('carrito');
+ const listaCursos = document.getElementById('lista-cursos');
+
+/**
+ * 
+ *  Listener
+ * 
+ *  */
+
+
+/***
+ * 
+ *  Funciones
+ * 
+ *  */ 
